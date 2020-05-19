b34e4a46055d2d542f8b35ee08542eaf620ca933
Adrian Sun <adrian_sun@livingstone.com.au>
2020-05-20 09:32:12 +1000

    customer

---
+++ P03.Service/Customer.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P03.Service
+{
+    public class Customer
+    {
+        public static void HusbandAction()
+        {
+            Console.WriteLine("Husband  Action");
+        }
+
+        public static void WifeAction()
+        {
+            Console.WriteLine("Wife  Action");
+        }
+
+        public static void SonAction()
+        {
+            Console.WriteLine("Son   Action");
+        }
+        public static void MorePersonAction()
+        {
+            Console.WriteLine("More Person Action");
+        }
+        public static void MoreChildAction()
+        {
+            Console.WriteLine("More Child Action");
+        }
+        public static void MoreDogAction()
+        {
+            Console.WriteLine("More Dog Action");
+        }
+
+
+    }
+}
