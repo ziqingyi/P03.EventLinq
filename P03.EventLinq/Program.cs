301c67051536107a95eefea5fd430b4d938eb348
Adrian Sun <adrian_sun@livingstone.com.au>
2020-05-25 09:10:47 +1000

    linq etension to get some random items, no duplicates.

--- P03.EventLinq/Program.cs
+++ P03.EventLinq/Program.cs
@@ -10,9 +10,24 @@ namespace P03.EventLinq
     {
         static void Main(string[] args)
         {
-            ShowPerformMethod.Show();
+
+            {
+                Console.WriteLine("**********************Show Performance****************************");
+                ShowPerformMethod.Show();
+            }
+            
+
+            {
+                Console.WriteLine("********************Linq Extension to get random number*********************");
+                List<int> list = new List<int>()
+                {
+                    1, 3, 4, 5, 6, 7, 8, 9, 91, 44, 56, 46, 99, 190, 33, 23, 123, 12, 56
+                };
 
 
+
+
+            }
         }
     }
 }
---
+++ P03.Utility/LinqExtension.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P03.Utility
+{
+    public static class LinqExtension
+    {
+        public static List<T> GetRandomList<T>(this List<T> t, int length)
+        {
+            if (t == null || !t.Any())
+            {
+                return null;
+            }
+
+            var totalCount = t.Count;
+            if (totalCount < length)
+            {
+                return t;
+            }
+
+            List<int> randomList = GetRandomList4Int(length,totalCount);
+
+            List<T> result = new List<T>();
+            for (int i = 0; i < totalCount; i++)
+            {
+                if (!randomList.Contains(i))
+                {
+                    continue;
+                }
+                result.Add(t[i]);
+            }
+            return result;
+        }
+
+        private static List<int> GetRandomList4Int(int lenght, int maxValue)
+        {
+            Random rd = new Random();
+            List<int> randomList = new List<int>();
+
+            while (true)
+            {
+                if (randomList.Count >= lenght)
+                {
+                    break;
+                }
+
+                var randomIndex = rd.Next(maxValue);
+                if (randomList.Contains(randomIndex))
+                {
+                    continue;
+                }
+
+                randomList.Add(randomIndex);
+            }
+            return randomList;
+        }
+    }
+}
